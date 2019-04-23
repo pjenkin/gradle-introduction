3e0e4cde9c4fdf628a689d0affd6d17b8762306a
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-23 12:03:16 +0100

    Setup of Java with gradle

---
+++ poetry/Poetry.java
@@ -0,0 +1,26 @@
+package org.gradle.poetry
+
+public class Poetry {
+
+  public List caesar()
+  {
+    List<String> lines = new ArrayList<String>();
+    lines.add("Infamy! They've all got it infamy!");
+    lines.add("veni vidi vici");
+    lines.add("blah blah blah");
+    return lines;
+  }
+
+// iterate through all lines
+  public void emit(List<String> lines) {
+    for (String line: lines) {
+      System.out.println(codec.encode(line));
+    }
+  }
+
+  public static void main(String[] args) {
+    Poetry p = new Poetry();
+
+    p.emit(p.caesar());
+  }
+}
